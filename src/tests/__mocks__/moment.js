a62882adbed444a003e2be71ca2348fa6c9ba2c5
Jordan Brutus <dadestined1@Jordans-MBP.home>
2018-06-23 20:48:01 -0400

    Setup devDependencies and dist folder + a few more jest tests

---
+++ src/tests/__mocks__/moment.js
@@ -0,0 +1,6 @@
+const moment = require.requireActual('moment');
+
+// Mocked version of moment
+export default (timestamp = 0) => {
+    return moment(timestamp);
+};
--- src/tests/components/NotFoundPage.test.js
+++ src/tests/components/NotFoundPage.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
-import Header from '../../components/Header';
+import NotFoundPage from '../../components/NotFoundPage';
 
-test('should render Header correctly', () => {
-    const wrapper = shallow(<Header />);
+test('should render NotFoundPage correctly', () => {
+    const wrapper = shallow(<NotFoundPage />);
     expect(wrapper).toMatchSnapshot();
 });
